cda44a2699132d944887a5683e769ee7811daa65
YFCookie7 <mikeli1117@gmail.com>
2023-07-06 17:59:26 +0800

    optimize

--- cogs/commands/chat.py
+++ cogs/commands/chat.py
@@ -19,6 +19,44 @@ ydl_opts = {
     'outtmpl': "/audio/"+audio_naming,   # ~/audio/abc.mp3
 }
 
+def get_audio(yt_url):
+    # Check if the audio file already exists
+    if os.path.exists(json_file) and os.stat(json_file).st_size > 0:
+        with open(json_file, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+        for data in json_data:
+            if data.get('yt_url') == yt_url:
+                return data.get('audio_filename')
+    return "not found"
+
+def download_audio(yt_url):
+    # Download the audio file if it does not exist
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(yt_url, download=True)
+        title = info['title']
+        ext = info['ext']
+        audio_file = ydl.prepare_filename(info)
+        audio_filename = (f"{title}.{ext}")[:-5] + ".mp3"
+
+    # Save the audio data to json
+    data = {
+        'audio_filename': audio_filename,
+        'yt_url': yt_url
+    }
+    if os.path.exists(json_file) and os.stat(json_file).st_size > 0:
+        with open(json_file, 'r+', encoding='utf-8') as f:
+            json_data = json.load(f)
+            json_data.append(data)
+            f.seek(0)  # Move the file pointer to the beginning of the file
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+            f.truncate()  # Truncate the file to remove any remaining content
+    else:
+        # Create a new JSON file with the data
+        with open(json_file, 'w', encoding='utf-8') as f:
+            json.dump([data], f, ensure_ascii=False, indent=4)
+    return audio_filename
+
+
 class Chat(commands.Cog):
     
     def __init__(self, bot):
@@ -55,42 +93,10 @@ class Chat(commands.Cog):
         if ctx.voice_client and ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
-        # Check if the audio file already exists
-        if os.path.exists(json_file) and os.stat(json_file).st_size > 0:
-            with open(json_file, 'r', encoding='utf-8') as f:
-                json_data = json.load(f)
-        for data in json_data:
-            if data.get('yt_url') == yt_url:
-                audio_filename = data.get('audio_filename')
-                channel = ctx.author.voice.channel
-                voice = ctx.voice_client or await channel.connect()
-                voice.play(discord.FFmpegPCMAudio(executable=FFMPEG_PATH, source=audio_dir + audio_filename))
-                return
-
-        # Download the audio file if it does not exist
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(yt_url, download=True)
-            title = info['title']
-            ext = info['ext']
-            audio_file = ydl.prepare_filename(info)
-            audio_filename = (f"{title}.{ext}")[:-5] + ".mp3"
-
-        # Save the audio data to json
-        data = {
-            'audio_filename': audio_filename,
-            'yt_url': yt_url
-        }
-        if os.path.exists(json_file) and os.stat(json_file).st_size > 0:
-            with open(json_file, 'r+', encoding='utf-8') as f:
-                json_data = json.load(f)
-                json_data.append(data)
-                f.seek(0)  # Move the file pointer to the beginning of the file
-                json.dump(json_data, f, ensure_ascii=False, indent=4)
-                f.truncate()  # Truncate the file to remove any remaining content
-        else:
-            # Create a new JSON file with the data
-            with open(json_file, 'w', encoding='utf-8') as f:
-                json.dump([data], f, ensure_ascii=False, indent=4)
+        audio_filename = get_audio(yt_url)
+        if audio_filename == "not found":
+            audio_filename = download_audio(yt_url)
+            
 
         channel = ctx.author.voice.channel
         voice = ctx.voice_client or await channel.connect()
